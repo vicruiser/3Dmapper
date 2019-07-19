577c913a40f92971af82ae48b371c406b2746765
vicruiser <victoria.ruiz.serra@gmail.com>
2019-07-19 15:59:10 +0200

    to date

---
+++ VEP_parser.py
@@ -0,0 +1,30 @@
+#from __future__ import print_function
+
+#import sys
+#import os
+import gzip
+#import re
+#import pkg_resources
+#import click
+#import locale
+
+# from codecs import open, getreader
+
+
+# from vcf_parser import (Genotype, HeaderParser)
+# from vcf_parser.utils import (format_variant, split_variants)
+
+####            Parser:         ####
+
+class VEPparser(object):
+
+
+def main():
+    
+    print("hola")
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
