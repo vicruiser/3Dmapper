a9c179f9894739469caed6acd228d43f8c35348c
vicruiser <victoria.ruiz.serra@gmail.com>
2019-07-22 11:41:14 +0200

    to date

--- VEP_parser.py
+++ VEP_parser.py
@@ -1,8 +1,10 @@
 #from __future__ import print_function
 
-#import sys
-#import os
+import sys
+import os
 import gzip
+#import pandas as pd
+#import dask.dataframe as dd
 #import re
 #import pkg_resources
 #import click
@@ -16,13 +18,22 @@ import gzip
 
 ####            Parser:         ####
 
-class VEPparser(object):
+# class VEPparser(object):
+
+# def joinDataFrames(vcf_file, interfaces_db):
+#     # Read the CSVs
+#     df1 = dd.read_csv(vcf_file)
+#     df2 = dd.read_csv(interfaces_db)
+
+#     # Merge them
+#     df = dd.merge(df1, df2, on='Bin_ID').compute()
+
+#     # Save the merged dataframe
+#     df.to_csv('merged.csv', index=False)
 
 
 def main():
-    
-    print("hola")
-    
+    print(" defenestrar [Conjugar el verbo defenestrar] Del lat de- 'de arriba abajo' y fenestra 'ventana' 1 tr Arrojar a alguien por una ventana 2 tr Destituir o expulsar a alguien de un puesto, cargo, situacion, etc")
     return
 
 
