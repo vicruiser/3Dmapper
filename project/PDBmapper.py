634f4059d4d335444b18808dff907686ba8b53f4
vicruiser <victoria.ruiz.serra@gmail.com>
2019-09-06 16:30:49 +0200

    main function

--- project/PDBmapper.py
+++ project/PDBmapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # Import necesary modules 
@@ -18,25 +18,21 @@ import parse_argv
 #protID = 'ENSP00000482258'
 #int_db_file = '/home/vruizser/PhD/2018-2019/Immunity_interfaces_analysis/raw_data/interfaces_mapped_to_v94.csv'
 
-def interfaceParse (interfacesDB_filepath, protID):
-    """Generate setID file.
+def interfaceParse (interfacesDB, protID):
+    """Parse input interfaces database to put it in the right format.
     
     Parameters
     ----------
     protID : str
-        input path of the VEP file
+        Ensemble protein id 
     interfacesDB_filepath : str
-        chosen name of the output file
+        
 
     Returns
     -------
-    setID.File
+    subset_interfaces_db
         write data frame to a txt file with two columns: one is the gene ids and the other one the VEP file
-    data.frame 
-        kasklkssklasdkmaskldmakmdalkmd
     """
-        # read interfaces file
-    interfacesDB = open(interfacesDB_filepath, "r")
     # get colnames of interfaces file
     intDB_colnames = interfacesDB.readline().strip().split(" ")
     # parse information related to protein ID
@@ -63,8 +59,24 @@ def interfaceParse (interfacesDB_filepath, protID):
     subset_prot_interface.rename(columns={'mapped_real_pos':'Protein_position'}, inplace=True)
 
     subset_prot_interface['region_id'] = subset_prot_interface['pdb_id'] + '_' + subset_prot_interface['ensembl_prot_id'] + '_' + subset_prot_interface['temp_chain'] + '_' + subset_prot_interface['int_chain'] + '_' + subset_prot_interface['interaction']
+    
+    return subset_prot_interface
 
-def PDBmapper(protID, interfacesDB_filepath, annovar):
+def vcfParser (annovar_filepath, protID):
+    biomartdb = open("/home/vruizser/PhD/2018-2019/git/PDBmapper/project/gene_transcript_protein_ens_ids.txt", "r")
+    ensemblIDs = mt.ensemblID_translator(biomartdb, protID)
+
+    # get the corresponding VEP file
+    crossref_file = open("/home/vruizser/PhD/2018-2019/git/PDBmapper/project/geneids_VEPfiles_crossref.txt", "r")
+    VEP_filename = mt.VEP_getter(crossref_file, ensemblIDs["geneID"])
+    VEP_dir= "/home/vruizser/PhD/2018-2019/PanCancer_data/vep_output/"
+    VEPfile = open(VEP_dir + VEP_filename, "r")
+    geneID = ensemblIDs['geneID']
+    cols = pd.read_csv(VEPfile, nrows = 0, skiprows = 42, sep = "\t").columns
+    VEP = mt.parser(VEPfile,  geneID, cols, "\t" )
+    return VEP
+
+def PDBmapper(protID, interfacesDB, annovar):
     """Generate setID file.
     
     Parameters
@@ -82,26 +94,7 @@ def PDBmapper(protID, interfacesDB_filepath, annovar):
         kasklkssklasdkmaskldmakmdalkmd
     """
 
-
-    biomartdb = open("/home/vruizser/PhD/2018-2019/git/PDBmapper/project/gene_transcript_protein_ens_ids.txt", "r")
-    ensemblIDs = mt.ensemblID_translator(biomartdb, protID)
-
-
-
-    # get the corresponding VEP file
-    crossref_file = open("/home/vruizser/PhD/2018-2019/git/PDBmapper/project/geneids_VEPfiles_crossref.txt", "r")
-    VEP_filename = mt.VEP_getter(crossref_file, ensemblIDs["geneID"])
-
-
-
-    VEP_dir= "/home/vruizser/PhD/2018-2019/PanCancer_data/vep_output/"
-    VEPfile = open(VEP_dir + VEP_filename, "r")
-    geneID = ensemblIDs['geneID']
-    cols = pd.read_csv(VEPfile, nrows = 0, skiprows = 42, sep = "\t").columns
-    VEP = mt.parser(VEPfile,  geneID, cols, "\t" )
-
-
-    setID = MapVariantToPDB(VEP, subset_prot_interface, 'region_id')
+    setID = mt.MapVariantToPDB(annovar, interfacesDB, 'region_id')
     setID.to_csv("setID.File")
 
 # define main function to execute the previous defined functions together
@@ -114,34 +107,39 @@ def main():
     if args.intdb:
         interfacesDB_file = args.intdb
         try:
-            interfacesDB = open(interfacesDB_file)
+            interfacesDB = open(interfacesDB_file, 'r')
+            interfacesDB = interfaceParse(interfacesDB, args.protid)
         except IOError:
             print ("ERROR: cannot open or read input interfaces db file:")
             exit(-1)
     else: 
-        interfacesDB_file = open("PDBmapper/database/interfaces.csv")
+        print ("Default interfaces DB is used.")
+        interfacesDB_file = open("./PDBmapper/dbs/interfaces.csv", 'r')
 
+    # set annovar:
     if args.annovar == "vep": 
-        print("let's run VEP")
+        print("VEP option will be available soon. Using VarMap db instead. Provide a vcf file please otherwise.")
+        annovar_file =  open("output_vep.txt", 'r')
     elif args.annovar == "vcf":
         try: 
-            open(args.annovar)
-            "do something"
+            print ("VarMap db is used.")
+            annovar_file = open(args.annovar, 'r')
+            annovar = vcfParser(annovar_file, args.protID)
         except IOError:
             print ("ERROR: cannot open or read input vcf file:")
             exit(-1)
-
     elif args.annovar == "varmap": 
         try: 
-            open(args.annovar)
-            "do something"
+            print ("VarMap db is used.")
+            annovar_file =  open("./PDBmapper/dbs/VarMap.csv", 'r')
+            annovar = vcfParser(annovar_file, args.protID)
         except IOError:
             print ("ERROR: cannot open or read input VarMap db file:")
             exit(-1)
-
-    PDBmapper(args.protid, interfacesDB, args.annovar)
     
-    return
+    # run PDBmapper
+    PDBmapper(args.protid, interfacesDB, annovar)
+
 
 
 ##########################
--- project/parse_argv.py
+++ project/parse_argv.py
@@ -1,56 +1,42 @@
 #!/usr/bin/python3
-# for commandline options:
-#from optparse import OptionParser, OptionGroup
 import argparse
-from numpy import mean
-import sys
-#import urllib2
-import os # This library contains functions which enables to check if a directory exists
-import re
+
+
 def parse_commandline():
-    
-    prog='pdb_mapper'
-    usage = "%prog <fasta> [options]"
-    version = "0.0a"
+
     description = \
-        "%prog map ."
+        "%(prog)s maps rare variants to protein interfaces data in 3D."
     epilog = \
         "Copyright (c) 2019 Victoria Ruiz -- "\
         "vruizser@bsc.es -- https://www.bsc.es/ruiz-serra-victoria-isabel"\
         ""
-
-    parser = argparse.ArgumentParser(prog=prog,  epilog = epilog, description = description)                 
-
+    # innit parser
+    parser = argparse.ArgumentParser( epilog = epilog, description = description )                 
+    
+    # vcf file
     annovar_group = parser.add_mutually_exclusive_group()
-    annovar_group.add_argument('--vcf',  dest = "annovar",  
+    annovar_group.add_argument('-vcf',   metavar = "<file>",  
                             help='input variant annotations (vcf)')
-    annovar_group.add_argument("--vep",   metavar="<file>", dest = "annovar",  
+    annovar_group.add_argument("-vep",   metavar="<file>",  
                             help="input variant file to annotate them with VEP")
-    annovar_group.add_argument("--varmap", dest = "annovar", default = 'annovar',
-                            help="use VarMap db of annotated variants", nargs='?',
-                            const='value_if_noarg')
+    annovar_group.add_argument("-varmap", action='store_true',
+                            help="use VarMap db of annotated variants", )
     parser.set_defaults(annovar = "varmap")
-
+    
+    # interfaces database file
     parser.add_argument("--intdb",  dest="intdb", metavar="<file>",
                     help="Interfaces database")
     parser.set_defaults(intdb=None)
 
-    parser.add_argument("--protid",  dest="protein_id",  action="store_true",
-                    help="Ensembl protein id")
-    parser.set_defaults(protein_id=False)
+    # protein id string
+    parser.add_argument("--protid",  metavar = "<String>"  ,
+                    help="Ensembl protein id", required = True)
     
+    # store arguments into variable
     args = parser.parse_args()
-
-    # raise an exception if we weren't able to find a match
-    if args.intdb is None:
-        print ("Default interfaces DB is used.")
-
-    if args.annovar == "varmap":
-        print ("VarMap db is used.")
-
-    print(args.annovar)
 
     # # clean up (recommended):
     del(parser)
-    #return print(args)
+    
     return args
+parse_commandline()
