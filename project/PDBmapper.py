5b81057f7c7897c2ad1e15c9e63328804055e33b
vicruiser <victoria.ruiz.serra@gmail.com>
2019-09-13 15:58:40 +0200

    pep8

--- project/PDBmapper.py
+++ project/PDBmapper.py
@@ -14,15 +14,9 @@ from VEPcrossref import VEPfileCrossrefGenerator as cr
 import parse_argv
 
 
-
 # Extract the info corresponding to the prot ID (Interface parse)
-
-#protID = 'ENSP00000482258'
-#int_db_file = './dbs/interfaces_mapped_to_v94.csv'
-
-def interfaceParse (interfacesDB, protID):
-    """Parse input interfaces database to put it in the right format.
-    
+def interfaceParse(interfacesDB, protID):
+    '''Parse input interfaces database to put it in the right format.
     Parameters
     ----------
     protID : str
@@ -33,15 +27,15 @@ def interfaceParse (interfacesDB, protID):
     -------
     subset_interfaces_db
         DESCRIPTION MISSING!!
-    """
+    '''
     # get colnames of interfaces file
-    intDB_colnames = interfacesDB.readline().strip().split(" ")
+    intDB_colnames = interfacesDB.readline().strip().split(' ')
     # parse information related to protein ID
-    prot_interface = mt.parser(input_file = interfacesDB,
-                                ensemblID = protID,
-                                colnames =  intDB_colnames,
-                                sep = " " )
-    # store 
+    prot_interface = mt.parser(input_file=interfacesDB,
+                               ensemblID=protID,
+                               colnames=intDB_colnames,
+                               sep=' ')
+    # store subspace
     subset_prot_interface = prot_interface[['pdb.id',
                                             'ensembl.prot.id',
                                             'temp.chain',
@@ -49,23 +43,36 @@ def interfaceParse (interfacesDB, protID):
                                             'interaction',
                                             'resid_sseq',
                                             'mapped.real.pos',
-                                            'pdb.pos' ]]
+                                            'pdb.pos']]
     # put it into right format
-    subset_prot_interface.columns = subset_prot_interface.columns.str.replace("\\.", "_")
+    subset_prot_interface.columns = \
+        subset_prot_interface.columns.str.replace('\\.', '_')
 
-    for col in ("resid_sseq", "mapped_real_pos", "pdb_pos"):
-        subset_prot_interface.loc[ : , col] = subset_prot_interface[col].str.replace("-", ',')
-        subset_prot_interface.loc[ : , col] = subset_prot_interface[col].str.split(',')
+    for col in ('resid_sseq', 'mapped_real_pos', 'pdb_pos'):
+        subset_prot_interface.loc[:, col] = \
+            subset_prot_interface[col].str.replace('-', ',')
+        subset_prot_interface.loc[:, col] = \
+            subset_prot_interface[col].str.split(',')
 
-    subset_prot_interface = mt.explode(subset_prot_interface, ["resid_sseq", "mapped_real_pos", "pdb_pos"])
-    subset_prot_interface.rename(columns={'mapped_real_pos':'Protein_position'}, inplace=True)
-    subset_prot_interface['region_id'] = subset_prot_interface['pdb_id'] + '_' + subset_prot_interface['ensembl_prot_id'] + '_' + subset_prot_interface['temp_chain'] + '_' + subset_prot_interface['int_chain'] + '_' + subset_prot_interface['interaction']
-
+    subset_prot_interface = mt.explode(subset_prot_interface,
+                                       ['resid_sseq',
+                                        'mapped_real_pos',
+                                        'pdb_pos'])
+    subset_prot_interface.rename(columns={'mapped_real_pos':
+                                          'Protein_position'}, inplace=True)
+    
+    subset_prot_interface['region_id'] = subset_prot_interface['pdb_id'] + \
+        '_' + subset_prot_interface['ensembl_prot_id'] + '_' + \
+        subset_prot_interface['temp_chain'] + '_' + \
+        subset_prot_interface['int_chain'] + '_' + \
+        subset_prot_interface['interaction']
+    
     return subset_prot_interface
 
+
 def vcfParser(VCF_file, geneID, *args):
-    """Parse input interfaces database to put it in the right format.
-    
+    '''Parse input interfaces database to put it in the right format.
+
     Parameters
     ----------
     VEP_file : str
@@ -76,52 +83,55 @@ def vcfParser(VCF_file, geneID, *args):
     -------
     VCF_subset
         DESCRIPTION MISSING!!
-    """
+    '''
 
-    if  args[0] == "vcf":
-        cols = pd.read_csv(VCF_file, nrows = 0, skiprows = 42,  sep = "\t").columns
-        VCF_file= open(VCF_file, 'r')
-        VCF_subset = mt.parser(input_file = VCF_file,
-                        ensemblID = geneID,
-                        colnames = cols,
-                        sep = "\t" )
+    if args[0] == 'vcf':
 
+        cols = pd.read_csv(VCF_file, nrows=0, skiprows=42, sep='\t').columns
+        VCF_file = open(VCF_file, 'r')
+        VCF_subset = mt.parser(input_file=VCF_file,
+                               ensemblID=geneID,
+                               colnames=cols,
+                               sep='\t')
         return VCF_subset
-    elif args[0] == "varmap": 
-        cols = pd.read_csv(VCF_file, nrows = 0, sep = "\t").columns
-        VCF_file= open(VCF_file, 'r')
-        VCF_subset = mt.parser(input_file = VCF_file,
-                        ensemblID = geneID,
-                        colnames = cols,
-                        sep = "\t" )
+    elif args[0] == 'varmap': 
+        cols = pd.read_csv(VCF_file, nrows=0, sep='\t').columns
+        VCF_file = open(VCF_file, 'r')
+        VCF_subset = mt.parser(input_file=VCF_file,
+                               ensemblID=geneID,
+                               colnames=cols,
+                               sep='\t')
         # drop columns
-        VCF_subset = VCF_subset[["CHROMOSOME",
-                                "COORDS",
-                                "USER_BASE",
-                                "USER_VARIANT",
-                                "ENSEMBL_BASE",
-                                "VEP_CODING_BASE",
-                                "GENE",	
-                                "GENE_ACC",
-                                "TRANSCRIPT",
-                                "CODON_CHANGE",
-                                "VEP_AA",
-                                "UNIPROT_AA",
-                                "AA_CHANGE",
-                                "CHANGE_TYPE",
-                                "RES_NAME",
-                                "RES_NUM"]]
+        VCF_subset = VCF_subset[['CHROMOSOME',
+                                 'COORDS',
+                                 'USER_BASE',
+                                 'USER_VARIANT',
+                                 'ENSEMBL_BASE',
+                                 'VEP_CODING_BASE',
+                                 'GENE',
+                                 'GENE_ACC',
+                                 'TRANSCRIPT',
+                                 'CODON_CHANGE',
+                                 'VEP_AA',
+                                 'UNIPROT_AA',
+                                 'AA_CHANGE',
+                                 'CHANGE_TYPE',
+                                 'RES_NAME',
+                                 'RES_NUM']]
         VCF_subset.drop_duplicates()
         VCF_subset = VCF_subset.rename(columns={'RES_NUM': 'Protein_position'})
-        VCF_subset['#Uploaded_variation'] = VCF_subset['CHROMOSOME'] + '_' + VCF_subset['COORDS'] + '_' + VCF_subset['USER_BASE'] + '_' + VCF_subset['USER_VARIANT'] 
+        VCF_subset['#Uploaded_variation'] = VCF_subset['CHROMOSOME'] + '_' + \
+            VCF_subset['COORDS'] + '_' + VCF_subset['USER_BASE'] + '_' +\
+            VCF_subset['USER_VARIANT']
+
         return VCF_subset
-    else: 
-        print("Error, wrong input vcf file.")
-    
+    else:
+        print('Error, wrong input vcf file.')
+
 
 def PDBmapper(protID, interfacesDB, VCF_subset, output_dir):
-    """Generate setID file.
-    
+    '''Generate setID file.
+  
     Parameters
     ----------
     protID : str
@@ -134,28 +144,32 @@ def PDBmapper(protID, interfacesDB, VCF_subset, output_dir):
     Returns
     -------
     setID.File
-        write data frame to a txt file with two columns. One is the gene ids and the other one the VEP file
+        write data frame to a txt file with two columns. One is the gene ids \
+             and the other one the VEP file
     MappedVariants.File
         more into
-    """
+    '''
 
     # Merge them both files
-    mapped_variants = pd.concat([VCF_subset, interfacesDB], axis = 1, join='inner')
-    # stop if there are no results 
-    if mapped_variants.empty: 
-        print ("Warning:", protID , "does not map with any annotated variant.")
-        exit (-1)
-    # if merging was successful, create setID file and 
+    mapped_variants = pd.concat([VCF_subset, interfacesDB],
+                                axis=1, join='inner')
+    # stop if there are no results
+    if mapped_variants.empty:
+        print('Warning:', protID, 'does not map with any annotated variant.')
+        exit(-1)
+    # if merging was successful, create setID file and
     # save the merged dataframe as well
-    else: 
-        setID_file = mapped_variants[["region_id",
-                        '#Uploaded_variation']]
+    else:
+        setID_file = mapped_variants[['region_id',
+                                      '#Uploaded_variation']]
         setID_file = setID_file.drop_duplicates()
-        # Save the merged dataframe, appending results and not reapeting headers
+        # Save the merged dataframe, appending results and not
+        #  reapeting headers
         with open(output_dir + 'setID.File', 'a') as f:
-            setID_file.to_csv(f, sep = " ", index=False,  header=f.tell()==0)
+            setID_file.to_csv(f, sep=' ', index=False,  header=f.tell() == 0)
         with open(output_dir + 'MappedVariants.File', 'a') as f:
-            mapped_variants.to_csv(f, sep = " ", index=False,  header=f.tell()==0)
+            mapped_variants.to_csv(f, sep=' ', index=False,
+                                   header=f.tell() == 0)
 
 
 # define main function to execute the previous defined functions together
@@ -164,47 +178,46 @@ def main():
     args = parse_argv.parse_commandline()
 
     # set VCF_subset:
-    if args.vep: 
-        print("""VEP option will be available soon. Using VarMap db instead.
-        Otherwise, please provide your own vcf file with the -vcf option.""")
-        try: 
-            print ("VarMap db is used.")
-            ClinVarDB =  "./dbs/ClinVar"
-            VCF_subset = vcfParser(ClinVarDB, args.protid, "varmap")
+    if args.vep:
+        print('''VEP option will be available soon. Using VarMap db instead.
+        Otherwise, please provide your own vcf file with the -vcf option.''')
+        try:
+            print('VarMap db is used.')
+            ClinVarDB = './dbs/ClinVar'
+            VCF_subset = vcfParser(ClinVarDB, args.protid, 'varmap')
         except IOError:
-            print ("ERROR: cannot open or read input VarMap db file.")
+            print('ERROR: cannot open or read input VarMap db file.')
             exit(-1)
     elif args.vcf:
         try: 
-            print ("vcf. file provided")
-            biomartdb = open("./dbs/gene_transcript_protein_ens_ids.txt", "r")
-            print ("Biomart file read...")
+            print('vcf. file provided')
+            biomartdb = open('./dbs/gene_transcript_protein_ens_ids.txt', 'r')
+            print('Biomart file read...')
             # get geneID
             ensemblIDs = mt.ensemblID_translator(biomartdb, args.protid)
             geneID = ensemblIDs['geneID']
-            print ("We have the gene ID...", geneID)
+            print('We have the gene ID...', geneID)
             # get the corresponding VEP file location
-            crossref_file = open("dbs/geneids_VEPfiles_crossref.txt", "r")
-            print ("We have the crossref file...", crossref_file)
+            crossref_file = open('dbs/geneids_VEPfiles_crossref.txt', 'r')
+            print('We have the crossref file...', crossref_file)
             # read VEP file
             VEP_filename = mt.VEP_getter(crossref_file, geneID)
-            print("We have the VEP filename", VEP_filename)
-            VEP_dir= args.vcf
+            print('We have the VEP filename', VEP_filename)
+            VEP_dir = args.vcf
             VEP_file = VEP_dir + '/' + VEP_filename
-            print("We have the VEP file", VEP_file)
+            print('We have the VEP file', VEP_file)
             # get subset VEP file
             VCF_subset = vcfParser(VEP_file, geneID, 'vcf')
         except IOError:
-            print ("ERROR: cannot open or read input vcf file.")
+            print('ERROR: cannot open or read input vcf file.')
             exit(-1)
-
-    elif args.varmap: 
-        try: 
-            print ("VarMap db is used.")
-            ClinVarDB =  "./dbs/ClinVar"
-            VCF_subset = vcfParser(ClinVarDB, args.protid, "varmap")
+    elif args.varmap:
+        try:
+            print('VarMap db is used.')
+            ClinVarDB = './dbs/ClinVar'
+            VCF_subset = vcfParser(ClinVarDB, args.protid, 'varmap')
         except IOError:
-            print ("ERROR: cannot open or read input VarMap db file.")
+            print('ERROR: cannot open or read input VarMap db file.')
             exit(-1)
 
     # set interfacesDB_susbet:
@@ -212,33 +225,27 @@ def main():
         try:
             interfacesDB = open(args.intdb, 'r')
         except IOError:
-            print ("ERROR: cannot open or read input interfaces db file.")
+            print('ERROR: cannot open or read input interfaces db file.')
             exit(-1)
-    else: 
-        print ("Default interfaces DB is used.")
-        interfacesDB_file = open("./dbs/interfaces_mapped_to_v94.csv", 'r')
-        interfacesDB_subset = interfaceParse(interfacesDB_file, args.protid)
-    
-
-    
+    else:
+        print('Default interfaces DB is used.')
+        interfacesDB_file = open('./dbs/interfaces_mapped_to_v94.csv', 'r')
+        interfacesDB_subset = interfaceParse(interfacesDB_file, args.protid) 
     # set default output dir:
     if args.out is None:
-        args.out = "./out/"
-    
+        args.out = './out/'
+
     # create chimera scripts:
     if args.chimera is not None:
         #chimera()
         pass
-    
     # run PDBmapper
     if args.protid:
         try:
             PDBmapper(args.protid, interfacesDB_subset, VCF_subset, args.out)
         except IOError:
-            print ("ERROR: Ensembl protein id provided is no supported.")
+            print('ERROR: Ensembl protein id provided is no supported.')
             exit(-1)
-    
-
 
 ##########################
 # execute main function  #
@@ -248,8 +255,5 @@ if __name__ == '__main__':
     start = timer()
     main()
     end = timer()
-    finish= end - start
-    print("Congratulations!. PDBmapper has run in", finish, "seconds.") 
-
-
-
+    finish = end - start
+    print('Congratulations!. PDBmapper has run in', finish, 'seconds.')
--- project/parse_argv.py
+++ project/parse_argv.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import argparse, os
 
+
 def dir_path(dirName):
     # Create target Directory if don't exist
     if not os.path.exists(dirName):
         os.mkdir(dirName)
-        print("Directory" , dirName ,  "created.")
+        print("Directory", dirName,  "created.")
     else:    
-        print( dirName ,  "is an existing directory. Results will be written in there.")
+        print(dirName,
+              "is an existing directory. Results will be written in there.")
+
 
 def parse_commandline():
 
@@ -18,44 +21,40 @@ def parse_commandline():
         "vruizser@bsc.es -- https://www.bsc.es/ruiz-serra-victoria-isabel"\
         ""
     # innit parser
-    parser = argparse.ArgumentParser( epilog = epilog, description = description )                 
+    parser = argparse.ArgumentParser(epilog=epilog, description=description)                 
     
     # vcf file
     annovar_group = parser.add_mutually_exclusive_group()
-    annovar_group.add_argument('-vcf',   metavar = "<String>",  dest = "vcf",
-                            help='input directory containing all annotated variants files (vcf)')
-    annovar_group.add_argument("-vep",   metavar="<file>",  dest = "vep",
-                            help="default VEP input")
-    annovar_group.add_argument("-varmap", action='store_true', dest = "varmap",
-                            help="use ClinVar db of annotated variants", default = "varmap" )
+    annovar_group.add_argument('-vcf', metavar="<String>", dest="vcf",
+                               help='input directory containing all \
+                                annotated variants files (vcf)')
+    annovar_group.add_argument("-vep",   metavar="<file>",  dest="vep",
+                               help="default VEP input")
+    annovar_group.add_argument("-varmap", action='store_true', dest="varmap",
+                               help="use ClinVar db of annotated variants",
+                               default="varmap")
     #parser.set_defaults(annovar = "varmap")
     
     # interfaces database file
-    parser.add_argument("-intdb",  dest="intdb", metavar="<file>",
-                    help="Interfaces database")
+    parser.add_argument("-intdb", dest="intdb", metavar="<file>",
+                        help="Interfaces database")
     parser.set_defaults(intdb=None)
 
     # protein id string
-    parser.add_argument("-protid",  metavar = "<String>"  , dest= "protid",
-                    help="Ensembl protein id", required = True)
+    parser.add_argument("-protid", metavar="<String>", dest="protid",
+                        help="Ensembl protein id", required=True)
 
     # create chimera script to visualize the region of interest
-    parser.add_argument("-chimera",  action = "store_true", dest = "chimera",
-                    help="generates chimeraX script")
+    parser.add_argument("-chimera", action="store_true", dest="chimera",
+                        help="generates chimeraX script")
     
     # create default output directory
-    parser.add_argument("-out",  metavar = "<String>"  , dest = "out",
-                    help="output directory", type=dir_path )
+    parser.add_argument("-out", metavar="<String>", dest="out",
+                        help="output directory", type=dir_path)
     parser.set_defaults(out="./out/")
     # store arguments into variable
     args = parser.parse_args()
 
     # # clean up (recommended):
     del(parser)
-    
     return args
-
-
-
-
-
