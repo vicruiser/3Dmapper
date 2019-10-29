c39f006680cf3c45013e56cc69d0451bccc3d1ce
vicruiser <victoria.ruiz.serra@gmail.com>
2019-10-29 14:04:02 +0100

    cleaning code

--- scripts/detect_vcf_format.py
+++ scripts/detect_vcf_format.py
@@ -4,13 +4,18 @@
 import vcfpy
 from scripts.decorator import tags
 
-@tags(text_start = "Detect inputs variants file format...\n",
-      text_succeed = "Detect inputs variants file format...done.\n",
-      text_fail = "Detect inputs variants file format...failed!\n",
+# add decorator to monitor function 
+@tags(text_start = "Detect input annotated genomic variants file format...\n",
+      text_succeed = "Detect input annotated genomic variants file format...done.\n",
+      text_fail = '''Detect input annotated genomic variants file format...failed!. \n \
+Please provide an file either in VCF or VEP format. \n''',
       emoji = "\U0001F50D ")
+
 def detect_format(infile):
-    '''Parse input and detect whether is a VCF or VEP file. Any other format
+    '''
+    Parse input and detect whether is a VCF or VEP file. Any other format
     is invalid. 
+    
     Parameters
     ----------
     infile : str        
@@ -18,45 +23,41 @@ def detect_format(infile):
     Returns
     -------
     infile_format : str 
-        Detected file format.
+        Detected file format (.vep or .vcf)
     '''
     try:
         # if vcf file, check if annotated with VEP
         vcf_reader = vcfpy.Reader.from_path(infile)
+        # check if "CSQ" INFO field in file
         if 'CSQ' not in vcf_reader.header.info_ids():
+            # input not recognized
             vcf_reader.close()
-            return print("ERROR: VCF is not VEP-annotated. Please annotate\
-                          the VCF with VEP before running this tool.")
-        else : 
-            print('Input file format is VCF')
-            infile_format = "vcf"
-            return infile_format
+            raise IOError()
+        else: 
+            # is vcf format
+            return "vcf"
     except:
-        print('The input file is not in vcf format.\
-               Checking if it is a VEP file...')
-        # define the columns that must be present in the vep file.  
+        # define the columns that must be present in a VEP file.  
         vep_header = set(['#Uploaded_variation',
-                                'Location',
-                                'Allele',
-                                'Gene',
-                                'Feature',
-                                'Feature_type',
-                                'Consequence',
-                                'cDNA_position',
-                                'CDS_position',
-                                'Protein_position',
-                                'Amino_acids',
-                                'Codons',
-                                'Existing_variation'])
+                          'Location',
+                          'Allele',
+                          'Gene',
+                          'Feature',
+                          'Feature_type',
+                          'Consequence',
+                          'cDNA_position',
+                          'CDS_position',
+                          'Protein_position',
+                          'Amino_acids',
+                          'Codons',
+                          'Existing_variation'])
         # open file to parse it     
-        vep_file = open(infile,'r')
-        # read lines to check if header exists in it 
+        vep_file = open(infile, 'r')
+        # read lines to check if header exists  
         for line in vep_file:
             res = vep_header.issubset(set(line.split()))
+            # if header found, is VEP format
             if res:
-                print("is vep format") 
-                infile_format = "vep"
-                return infile_format
-
-        raise IOError("Sorry, input format not recognized. Possible input\
-                     formats are .vep or .vcf")
+                return "vep"
+        # input not recognized
+        raise IOError()
