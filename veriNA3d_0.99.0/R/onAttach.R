6b7d4ffb269fe7ea2b21ab489f17dc078061dc3f
vruizser <victoria.ruiz.serra@gmail.com>
2023-04-12 17:04:56 +0200

    examples ready

--- veriNA3d_0.99.0/R/onAttach.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(lib, pkg) {
-    msg <- paste("\nThanks for using veriNA3d!\n",
-                "If you find it useful for your research, please cite us.\n",
-                "Find the details typing \"citation('veriNA3d')\".\n", sep="")
-    packageStartupMessage(msg)
-}
